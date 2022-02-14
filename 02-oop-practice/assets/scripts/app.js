0fbb9a08b347d971c88256b1035a491a2fce4ea7
nedkamburov <nkamb@softserveinc.com>
2022-02-14 11:23:43 +0200

    add drag&drop feature]

--- 02-oop-practice/assets/scripts/app.js
+++ 02-oop-practice/assets/scripts/app.js
@@ -44,21 +44,22 @@ class Tooltip extends Component {
 
     create() {
         const tooltipElement = document.createElement('div');
-        tooltipElement.className ='card';
+        tooltipElement.className = 'card';
         tooltipElement.textContent = 'Dummy!';
-        tooltipElement.addEventListener('click', this.detach );
+        tooltipElement.addEventListener('click', this.detach);
         this.element = tooltipElement;
     }
 }
 
-class ProjectItem{
+class ProjectItem {
     hasActiveTooltip = false;
 
-    constructor(id, updateProjectListFunction){
+    constructor(id, updateProjectListFunction) {
         this.id = id;
         this.updateProjectListHandler = updateProjectListFunction;
         this.connectMoreInfoButton();
         this.connectSwitchButton();
+        this.connectDrag();
     }
 
     showMoreInfoHandler() {
@@ -73,18 +74,29 @@ class ProjectItem{
         this.hasActiveTooltip = true;
     }
 
+    connectDrag() {
+        document.getElementById(this.id).addEventListener('dragstart', event => {
+            event.dataTransfer.setData('text/plain', this.id);
+            event.dataTransfer.effectAllowed = 'move';
+        })
+    }
+
     connectMoreInfoButton() {
         const projectItemElement = document.getElementById(this.id);
         const moreInfoBtn = projectItemElement.querySelector('button:first-of-type');
         moreInfoBtn.addEventListener('click', this.showMoreInfoHandler)
     }
 
-    connectSwitchButton(type){
+    connectSwitchButton(type) {
+
         const projectItemElement = document.getElementById(this.id);
         let switchBtn = projectItemElement.querySelector('button:last-of-type');
         switchBtn = DOMHelper.clearEventListeners(switchBtn);
-        switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
-        switchBtn.addEventListener('click',  this.updateProjectListHandler.bind(null, this.id))
+        if (type !== undefined) {
+            switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+        }
+        
+        switchBtn.addEventListener('click', this.updateProjectListHandler.bind(null, this.id))
     }
 
     update(updateProjectListFunction, type) {
@@ -93,16 +105,51 @@ class ProjectItem{
     }
 }
 
-class ProjectList{
+class ProjectList {
     projects = [];
 
     constructor(type) {
         this.type = type;
-        
+
         const projectItems = document.querySelectorAll(`#${type}-projects li`)
         for (const projectItem of projectItems) {
             this.projects.push(new ProjectItem(projectItem.id, this.switchProject.bind(this), this.type));
         }
+        this.connectDroppable();
+    }
+
+    connectDroppable() {
+        const list = document.querySelector(`#${this.type}-projects ul`);
+
+        list.addEventListener('dragenter', event => {
+            if (event.dataTransfer.types[0] === 'text/plain') {
+                list.parentElement.classList.add('droppable');
+                event.preventDefault();
+            }
+        })
+
+        list.addEventListener('dragover', event => {
+            if (event.dataTransfer.types[0] === 'text/plain') {
+                event.preventDefault();
+            }
+        })
+
+        list.addEventListener('dragleave', event => {
+            // A bit buggy in Safari, as relatedTarget is null
+            if (event.relatedTarget && event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+                list.parentElement.classList.remove('droppable');
+            }
+        })
+
+        list.addEventListener('drop', event => {
+            const projectId = event.dataTransfer.getData('text/plain');
+            if (this.projects.find(p => p.id === projectId)) {
+                return;
+            } 
+            document.getElementById(projectId).querySelector('button:last-of-type').click();
+            list.parentElement.classList.remove('droppable');
+            event.preventDefault();
+        })
     }
 
     setSwitchHandler(switchHandlerFunction) {
@@ -115,13 +162,13 @@ class ProjectList{
         project.update(this.switchProject.bind(this), this.type);
     }
 
-    switchProject(projectId) {        
+    switchProject(projectId) {
         this.switchHandler(this.projects.find(p => p.id === projectId));
         this.projects = this.projects.filter(p => p.id !== projectId);
     }
 }
 
-class App{
+class App {
     static init() {
         const activeProjectsList = new ProjectList("active");
         const finishedProjectsList = new ProjectList("finished");
